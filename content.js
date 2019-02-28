d3c517ed78e1dcb1315840d19e9a57008d9b75bf
Abin Simon <abinsimon10@gmail.com>
2019-02-28 17:28:10 +0530

    bugfix: multiple notification group support

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
 function highlightElements(highlightType) {
-  const lineRange = document.getElementsByClassName(
+  const lineRanges = Array.from(document.getElementsByClassName(
     "boxed-group-inner list-group notifications"
-  )[0];
-  const allLines = Array.from(lineRange.getElementsByClassName("type-icon"));
-  let noFade = allLines;
-  if (highlightType !== null)
-    noFade = Array.from(
-      lineRange.querySelectorAll('[aria-label="' + highlightType + '"]')
-    );
+  ));
+  lineRanges.forEach(lineRange => {
+    const allLines = Array.from(lineRange.getElementsByClassName("type-icon"));
+    let noFade = allLines;
+    if (highlightType !== null)
+      noFade = Array.from(
+        lineRange.querySelectorAll('[aria-label="' + highlightType + '"]')
+      );
 
-  allLines.forEach(line => {
-    if (noFade.indexOf(line) !== -1) {
-      line.parentElement.parentElement.style.opacity = 1;
-    } else {
-      line.parentElement.parentElement.style.opacity = 0.2;
-    }
+    allLines.forEach(line => {
+      if (noFade.indexOf(line) !== -1) {
+        line.parentElement.parentElement.style.opacity = 1;
+      } else {
+        line.parentElement.parentElement.style.opacity = 0.2;
+      }
+    });
   });
 }
 
